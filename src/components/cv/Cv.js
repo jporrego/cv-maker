9e391962aa1c22daf7d037d30e6cf32dad1827ef
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-05-04 18:40:59 -0400

    started defining main components

---
+++ src/components/cv/Cv.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Cv.css";
+import Introduction from "../introduction/Introduction";
+
+function Cv() {
+  return (
+    <div className="cv">
+      <Introduction></Introduction>
+    </div>
+  );
+}
+
+export default Cv;
---
+++ src/components/introduction/Introduction.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Introduction.css";
+
+function Introduction() {
+  return <div className="introduction">Introduction</div>;
+}
+
+export default Introduction;
