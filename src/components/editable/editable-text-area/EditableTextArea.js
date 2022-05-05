7f1373bf4625ebbd57da3bc18073216a4c2c02ac
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-05-05 15:55:02 -0400

    More work on editable text area logic

--- src/components/editable/editable-text-area/EditableTextArea.js
+++ src/components/editable/editable-text-area/EditableTextArea.js
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
+import "./EditableTextArea.css";
+import editIcon from "../../../assets/icons/edit-3.svg";
 
 function EditableTextArea({ text, setText }) {
   const [isFormVisible, setIsFormVisible] = useState(false);
-
+  const [editBtnVisisble, setEditBtnVisisble] = useState(false);
   const showForm = () => {
     setIsFormVisible(!isFormVisible);
   };
+
+  const showEditBtn = () => {
+    if (!editBtnVisisble) {
+      setEditBtnVisisble(true);
+    }
+  };
+
+  const hideEditBtn = () => {
+    const timer = setTimeout(() => {
+      setEditBtnVisisble(false);
+    }, 500);
+  };
+
+  const staticText = (
+    <div className="static-text">
+      <div onClick={showForm}>{text}</div>
+    </div>
+  );
+
+  const editableText = (
+    <div>
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <button type="button" onClick={showForm}>
+        Dones
+      </button>
+    </div>
+  );
+
   return (
-    <div onClick={showForm}>
-      {isFormVisible ? (
-        <textarea
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        ></textarea>
-      ) : (
-        <div>{text}</div>
-      )}
+    <div onMouseEnter={showEditBtn} onMouseLeave={hideEditBtn}>
+      {isFormVisible ? editableText : staticText}
     </div>
   );
 }
