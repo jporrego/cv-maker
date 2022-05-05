9e7842a1192cc3f136ac39780564f017d97000a7
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-05-05 18:25:03 -0400

    Start skills functionality

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skills.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-solid-svg-icons";
+import {
+  faJsSquare,
+  faHtml5,
+  faCss3Alt,
+  faReact,
+  faNode,
+  faPython,
+  faJava,
+  faPhp,
+  faGitAlt,
+} from "@fortawesome/free-brands-svg-icons";
 import SectionTitle from "../section-title/SectionTitle.js";
 
 function Skills() {
+  const [skills, setStills] = useState([faJsSquare, faHtml5]);
+
+  const skillItems = skills.map((skill) => {
+    return (
+      <FontAwesomeIcon key={skill.iconName} icon={skill} className="icon" />
+    );
+  });
+
   return (
     <div className="skills">
       <SectionTitle title={"skills"}></SectionTitle>
-      AUaosd alsdua sidu aoslid
+      <div className="icons">
+        {skillItems}
+        {/*
+        <FontAwesomeIcon icon={faJsSquare} className="icon" />
+        <FontAwesomeIcon icon={faHtml5} className="icon" />
+        <FontAwesomeIcon icon={faCss3Alt} className="icon" />
+        <FontAwesomeIcon icon={faReact} className="icon" />
+        <FontAwesomeIcon icon={faNode} className="icon" />
+        <FontAwesomeIcon icon={faPhp} className="icon" />
+        <FontAwesomeIcon icon={faPython} className="icon" />
+        <FontAwesomeIcon icon={faJava} className="icon" />
+        <FontAwesomeIcon icon={faGitAlt} className="icon" />*/}
+      </div>
     </div>
   );
 }
