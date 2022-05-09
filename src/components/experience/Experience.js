25589c0c5b34dfab9afe8c5f331f620ad03a0c14
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-05-09 18:04:50 -0400

    Changed Experience state to array of objects.

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -7,13 +7,25 @@ import uniqid from "uniqid";
 function Experience() {
   const [jobs, setJobs] = useState([]);
 
-  /*
+  useEffect(() => {
+    // Update the document title using the browser API
+    getJobs();
+  }, []);
+
   const getJobs = () => {
     const jobData = [
       {
         id: uniqid(),
         position: "Full Stack Developer",
         company: "Google",
+        date: "November 2022-Present",
+        description:
+          "Worked as a developer for a long time. Etc etc it as as dkasd.Worked as a developer for a long time. Etc etc it as as dkasd.Worked as a developer for a long time. Etc etc it as as ",
+      },
+      {
+        id: uniqid(),
+        position: "Full Stack Developer",
+        company: "Usurus",
         date: "November 2022-Present",
         description:
           "Worked as a developer for a long time. Etc etc it as as dkasd.Worked as a developer for a long time. Etc etc it as as dkasd.Worked as a developer for a long time. Etc etc it as as ",
@@ -28,7 +40,7 @@ function Experience() {
     ];
 
     setJobs(jobData);
-  };*/
+  };
 
   const addJob = () => {
     /*
@@ -41,24 +53,38 @@ function Experience() {
       const newKey = uniqid();
       setJobs([
         ...jobs,
-        <Job
-          key={newKey}
-          id={newKey}
-          position={"Position"}
-          company={"Company"}
-          date={"2020 - Present"}
-          description={"Description"}
-          onDelete={deleteJob}
-        ></Job>,
+        {
+          key: newKey,
+          id: newKey,
+          position: "Position",
+          company: "Company",
+          date: "2020 - Present",
+          description: "Description",
+          onDelete: { deleteJob },
+        },
       ]);
     }
   };
 
   function deleteJob(e, id) {
     e.preventDefault();
-    const newJobsArray = [...jobs].filter((job) => job.key != id);
+    const newJobsArray = [...jobs].filter((job) => job.id !== id);
     setJobs(newJobsArray);
   }
+
+  const jobElements = jobs.map((job) => {
+    return (
+      <Job
+        key={job.id}
+        id={job.id}
+        position={job.position}
+        company={job.company}
+        date={job.date}
+        description={job.description}
+        onDelete={deleteJob}
+      ></Job>
+    );
+  });
 
   return (
     <div className="experience">
@@ -68,7 +94,7 @@ function Experience() {
         btn={"+"}
         btnOnClick={addJob}
       ></SectionTitle>
-      {jobs.map((job) => job)}
+      {jobElements}
     </div>
   );
 }
