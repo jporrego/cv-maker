2bc75b1406b38fb29b288d5a5440742c92f6d487
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-05-11 15:20:20 -0400

    Profile component working

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,28 +4,61 @@ import "./Profile.css";
 function Profile() {
   const [name, setName] = useState("Sophia Porter");
   const [ocupation, setOcupation] = useState("Full Stack Developer");
+  const [phone, setPhone] = useState("(212)-456-7890");
+  const [email, setEmail] = useState("sporter@gmail.com");
   const [profilePicture, setProfilePicture] = useState("../../../assets");
   const [showForm, setShowForm] = useState(false);
 
   const staticText = (
-    <div className="profile-static-text" onClick={() => setShowForm(true)}>
+    <div className="profile-static-text">
       <div className="name-wrapper">
-        <div className="name">{name}</div>
+        <div className="name" onClick={() => setShowForm(true)}>
+          {name}
+        </div>
       </div>
-      <div className="ocupation">{ocupation}</div>
-      <div className="profile-pic"></div>
+      <div className="ocupation" onClick={() => setShowForm(true)}>
+        {ocupation}
+      </div>
+      <div className="contact" onClick={() => setShowForm(true)}>
+        {email}
+        {phone}
+      </div>
     </div>
   );
 
   const form = (
     <div className="profile-form">
-      <input type="text" />
-      <input type="text" />
-      <input type="file" name="profile-pic" id="profile-pic" />
+      <input
+        type="text"
+        className="profile-form-name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      />
+      <input
+        type="text"
+        className="profile-form-ocupation"
+        value={ocupation}
+        onChange={(e) => setOcupation(e.target.value)}
+        placeholder="Ocupation"
+      />
+      <div className="profile-form-contact">
+        <input
+          type="email"
+          className="profile-form-email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="tel"
+          className="profile-form-phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="Phone number"
+        />
+      </div>
       <div className="buttons">
-        <button className="btn-square btn--red" onClick={(e) => e}>
-          Delete
-        </button>
         <button
           className="btn-square btn--purple"
           onClick={() => setShowForm(false)}
